d1f19b1f80e8284a5b8bd6dbf611c3d8ce3d69fa
Pawel Furman <pf.furman@gmail.com>
2018-01-30 21:50:12 +0100

    ES6 to React solution

--- script.js
+++ script.js
@@ -1,79 +1,3 @@
-class Stopwatch {
-
-
-    constructor(display) {
-    	
-
-
-
-        this.running = false;
-        this.display = display;
-        this.reset();
-        this.print(this.times);
-    }
-
-    reset() {
-        this.times = {
-            minutes: 0,
-            seconds: 0,
-            miliseconds: 0
-        }
-    }
-
-    print() {
-        this.display.innerText = this.format(this.times);
-	}
-
-	format(times) {
-        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
-	}
-	start() {
-    if (!this.running) {
-        this.running = true;
-        this.watch = setInterval(() => this.step(), 10);
-    }
-}
-
-
-	step() {
-    if (!this.running) return;
-    this.calculate();
-    this.print();
-}
-
-
-	calculate() {
-    this.times.miliseconds += 1;
-    if (this.times.miliseconds >= 100) {
-        this.times.seconds += 1;
-        this.times.miliseconds = 0;
-    }
-    if (this.times.seconds >= 60) {
-        this.times.minutes += 1;
-        this.times.seconds = 0;
-    }
-}
-
-
-	stop() {
-    this.running = false;
-    clearInterval(this.watch);
-	}
-    
-}
-
-
-
-const stopwatch = new Stopwatch(
-document.querySelector('.stopwatch'));
-
-var startButton = document.getElementById('start');
-startButton.addEventListener('click', () => stopwatch.start());
-
-var stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', () => stopwatch.stop());
-
-
 function pad0(value) {
     let result = value.toString();
     if (result.length < 2) {
@@ -81,4 +5,94 @@ function pad0(value) {
     }
     return result;
 }
+
+class Counter extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            running: false,
+            watch: null,
+            times: {
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 0
+            },
+            results: []
+        }
+    }
+
+    format(times){
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
+
+    counterStart(){
+        if(!this.state.running){
+            this.setState({
+                running: true,
+                watch: setInterval(() => this.step(), 10)
+            })
+        }
+    }
+
+
+    counterStop(){ 
+        clearInterval(this.state.watch);
+        this.setState({
+            running: false,
+            watch: null
+        })
+    }
+
+    step(){
+        let miliseconds = this.state.times.miliseconds;
+        let seconds = this.state.times.seconds;
+        let minutes = this.state.times.minutes;
+    
+        miliseconds++;
+        if (miliseconds >= 100) {
+            seconds += 1;
+            miliseconds = 0;
+        }
+        if (seconds >= 60) {
+            minutes += 1;
+            seconds = 0;
+        }
+
+        this.setState({
+            times: {
+                miliseconds,
+                seconds,
+                minutes
+            }
+        })
+    }
+
+    saveResults(){
+        this.setState({
+            results: [...this.state.results, this.format(this.state.times)]
+        });
+    }
+    
+
+    renderResult(result, i){
+       return (<li key={i}>{result}</li>); 
+    }
+
+    render(){
+        return (<div>
+            <nav className="controls">
+              <span>Running: {this.state.running.toString()}</span>
+              <button href="#" className="button" onClick={this.counterStart.bind(this)}>Start</button>
+              <button href="#" className="button" onClick={this.counterStop.bind(this)}>Stop</button>
+              <button href="#" className="button" onClick={this.saveResults.bind(this)}>Save Results</button>
+            </nav>
+            <div className="stopwatch">{this.format(this.state.times)}</div>
+            <ul className="results">
+                {this.state.results.map( (r, i) => this.renderResult(r, i))}
+            </ul>
+        </div>)
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
 
